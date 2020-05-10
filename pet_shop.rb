4e04b2dadf8955234d930e2784eb00be84a3f448
Tino <tinodafunk@googlemail.com>
2020-05-10 21:09:46 +0100

    failed_homework_submission

---
+++ pet_shop.rb
@@ -0,0 +1,42 @@
+def pet_shop_name(petshop)
+  return petshop[:name]
+end
+
+
+def total_cash(cash_total)
+  return # Point to to pet_shop hash, admin hash, total_cash hash and return value
+end
+
+
+def add_or_remove_cash__add(cash)
+  return # Point to to pet_shop hash, admin hash, total_cash hash and add 10
+end
+
+
+def add_or_remove_cash__remove(cash)
+  return # Point to to pet_shop hash, admin hash, total_cash hash and subtract 10
+end
+
+#
+def pets_sold(pet_total)
+  return # Point to to pet_shop hash, then admin hash, pets_sold hash and return value
+end
+
+def increase_pets_sold(pet_total)
+return # Point to to pet_shop hash, admin hash, pets_sold hash and add 2
+end
+
+
+def stock_count(stock)
+  return # Point to pet_shop hash, then count number of arrays
+end
+
+
+def all_pets_by_breed__found(pets)
+  for # Point to pet_shop hash, pets array, search within this array for breed type. I don't understand how to enable the check for breed type to return a successful test
+end
+
+
+  def all_pets_by_breed__not_found(pets)
+    for # Point to pet_shop hash, pets array, search within this array for breed type. I don't understand how to enable a search for breed type to return a successful test
+  end
